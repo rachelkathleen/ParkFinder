c03b8d86ce4410366d74ee0b94b0c08647edbabf
rachelkathleen <rachel.hawa@gmail.com>
2019-03-26 15:37:02 -0500

    added first route

--- app/controllers/park_controller.rb
+++ app/controllers/park_controller.rb
@@ -4,4 +4,17 @@ class ParkController < ApplicationController
     set :public_folder, 'public'
     set :views, 'app/views/park_views'
   end
+
+  get '/parks' do
+    erb :parks
+  end
+
+
+  #trying to create dynamic route that will show the park name when a user
+  # clicks on the link to that park
+
+  # get '/parks/:park_name' do
+  #   @park = Park.find(params[:id])
+  #   erb :'/show'
+  # end
 end

3b3478e8302c3eb9fb86266e01c4ac4d08928e4b
rachelkathleen <rachel.hawa@gmail.com>
2019-03-26 14:15:01 -0500

    move has_secure_password to user model

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,5 @@
 require_relative '../../config/environment'
 
 class UserController < ApplicationController
-  has_secure_password
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views/user_views'
-  end
 end

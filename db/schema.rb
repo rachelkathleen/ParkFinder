8dfbc7f7d7247715783161fcbecbfb2ccc489281
rachelkathleen <rachel.hawa@gmail.com>
2019-03-26 17:38:26 -0500

    updated columns for notes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190326194620) do
+ActiveRecord::Schema.define(version: 20190326223015) do
 
   create_table "notes", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
+    t.string  "title"
+    t.string  "content"
+    t.integer "user_id"
+    t.integer "park_id"
   end
 
   create_table "parks", force: :cascade do |t|

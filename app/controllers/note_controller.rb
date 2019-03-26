f1f6eea3c819761c92878be421250a601c276e6b
rachelkathleen <rachel.hawa@gmail.com>
2019-03-26 17:39:20 -0500

    remove config section

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -1,7 +1,4 @@
 require_relative '../../config/environment'
 class NoteController < ApplicationController
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  
 end

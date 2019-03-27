044051a675de363ee2c03b77bf38cb785b434a67
rachelkathleen <rachel.hawa@gmail.com>
2019-03-27 16:18:03 -0500

    debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,14 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :homepage
   end
-
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      User.find(session[:user_id])
-    end
-  end
+  # 
+  # helpers do
+  #   def logged_in?
+  #     !!session[:user_id]
+  #   end
+  #
+  #   def current_user
+  #     User.find(session[:user_id])
+  #   end
+  # end
 end

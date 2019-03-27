c133181c955c13ee76ae3c1ec2827728e8add6a7
rachelkathleen <rachel.hawa@gmail.com>
2019-03-27 17:38:27 -0500

    add helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,21 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :session
+    enable :sessions
     set :session_secret, 'parks'
   end
 
   get "/" do
     erb :homepage
   end
-  # 
-  # helpers do
-  #   def logged_in?
-  #     !!session[:user_id]
-  #   end
-  #
-  #   def current_user
-  #     User.find(session[:user_id])
-  #   end
-  # end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+  end
 end

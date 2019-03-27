4fb501c81c5afc8724b71102547ae7454734010b
rachelkathleen <rachel.hawa@gmail.com>
2019-03-26 22:55:16 -0500

    fill in RESTful routes

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -2,31 +2,50 @@ require_relative '../../config/environment'
 class NoteController < ApplicationController
 
   get '/notes' do
-    #display all notes
+    @items = Item.all
+    erb :'items/index' #create this view
   end
 
   get 'notes/new' do
-    #create a new note
+    erb :'items/new' #create view
   end
 
   post 'notes' do
-    #Creates a note in DB, and then typically redirects
+    @note = Note.new(title: params[:title], content: params[:content])
+    #how to add user_id and park_id?
+    if @note.save
+        redirect "/notes/#{note.id}"
+    else
+        erb :'notes/new'
+    end
   end
 
   get 'notes/:id' do
-    #Show info about specific note
+    @note = Note.find(params[:id])
+    erb :'notes/show' #create this erb file
   end
 
   get 'notes/:id/edit' do
-    #show form to edit a specific note
+    @note = Note.find(params[:id])
+    erb :'notes/edit' #create this erb file
   end
 
   patch 'notes/:id' do
-    #edit note in DB the redirects
+    @note = Note.find(params[:id])
+    if @item.update(title: params[:title], content: params[:content])
+      redirect "/notes/#{@note.id}"
+    else
+      erb :'notes/edit'
+    end
   end
 
   delete 'notes/:id' do
-    #deletes a note in the DB then redirects
-  end
+    @note = Note.find(params[:id])
 
+    if @note.delete
+      redirect '/notes'
+    else
+      redirect "/notes/#{@note.id}"
+    end
+  end
 end

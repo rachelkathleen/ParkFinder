7ed69c156ee4b771930bd72a9a3acbebf8df88b2
rachelkathleen <rachel.hawa@gmail.com>
2019-03-27 22:20:56 -0500

    fix error

--- app/controllers/park_controller.rb
+++ app/controllers/park_controller.rb
@@ -12,17 +12,15 @@ class ParkController < ApplicationController
     erb :'park_views/show'
   end
 
+  get '/parks/been_to' do
+
+  end
+
+  get '/parks/bucket_list' do
+    
+  end
   # get '/parks/:park_name' do
   #   @park = Park.find_by(park_name: params[:park_name])
   #   erb :'/show'
   # end
 end
-
-# HTTP VERB          ROUTE               ACTION             DESCRIPTION
-#   GET             /artists             index       Shows all the artists in the db
-#   GET             /artists/new         new         Show a FORM for a new artist
-#   POST            /artists             create      Creates an artist in DB, and then typically redirects
-#   GET             /artists/:id         show        Show info about specific artist
-#   GET             /artists/:id/edit    edit        Show a FORM to edit a specific artist
-#   PUT/PATCH       /artists/:id         update      Edits an artist in DB, and then typically redirects
-#   DELETE          /artists/:id         destroy     Deletes an artist from DB, and then typically redirects

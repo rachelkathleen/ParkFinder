0ac216f75f96cc6e5eb0612915d734540bf90821
rachelkathleen <rachel.hawa@gmail.com>
2019-04-02 18:53:38 -0500

    change new route to mass assign and add been_to and bucket_list routes

--- app/controllers/user_parks_controller.rb
+++ app/controllers/user_parks_controller.rb
@@ -6,6 +6,15 @@ use Rack::Flash
   end
 
   post '/parks/user_parks/new' do
+    if params[:park_ids]
+      params[:park_ids].each do |park_id|
+        user_id = params[:user_id]
+        been_to = params[:been_to]
+        bucket_list = params[:bucket_list]
+        @userpark = UserPark.create(user_id: user_id, park_id: park_id, been_to: been_to, bucket_list: bucket_list)
+        end
+        redirect "/notes/new"
+      else
     @userpark = UserPark.new(params)
     if @userpark.save
         redirect "/notes/new"
@@ -13,11 +22,6 @@ use Rack::Flash
         erb :failure
     end
   end
-
-
-  get '/notes/:id/edit' do
-    @up = UserPark.find(params[:id])
-    erb :'note_views/edit'
   end
 
   patch '/parks/user_parks/:id' do
@@ -28,6 +32,15 @@ use Rack::Flash
       erb :'user_views/failure'
     end
   end
+
+  get '/user_parks/been_to' do
+    erb :'user_views/been_to'
+  end
+
+  get '/user_parks/bucket_list' do
+    erb :'user_views/bucket_list'
+  end
+
 
   delete '/parks/user_parks/:id' do
     @up = UserPark.find(params[:id])

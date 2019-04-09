80273c27c882f6a504dfd9b8303829f4a826f0f4
rachelkathleen <rachel.hawa@gmail.com>
2019-04-09 11:55:40 -0500

    add search by name function

--- app/controllers/park_controller.rb
+++ app/controllers/park_controller.rb
@@ -5,13 +5,22 @@ class ParkController < ApplicationController
   use Rack::Flash
 
   get '/parks' do
+    @parks = Park.all
     erb :'park_views/parks'
   end
+
+  post '/parks' do
+    @parks = Park.all.select{|park| park.park_name.downcase.include?(params[:park_name].downcase)}
+    erb :'park_views/parks'
+  end
+
 
   get '/parks/:id' do
     @park = Park.find(params[:id])
     erb :'park_views/show'
   end
+
+
 
   # get '/parks/:park_name' do
   #   @park = Park.find_by(park_name: params[:park_name])
